47f4e9c0ce49989aaa9ce971194e5169e85aa9f0
tong <newrtong@gmail.com>
2020-02-19 15:17:22 +0800

    init

---
+++ operator.go
@@ -0,0 +1,17 @@
+package main
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func divided(a float64, b float64) float64 {
+	return a / b
+}
---
+++ operator_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	if add(3, 5) != 8 {
+		t.Error("add not equal")
+	}
+}
+
+func TestSub(t *testing.T) {
+	if sub(10, 5) != 5 {
+		t.Error("1. sub result no Expected")
+	}
+	if sub(5, 6) != -1 {
+		t.Error("2. sub result no Expected")
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	if multiply(10, 5) != 50 {
+		t.Error("1. multiply result no Expected")
+	}
+	if multiply(0, 3) != 0 {
+		t.Error("2. multiply result no Expected")
+	}
+	if multiply(-1, 7) != -7 {
+		t.Error("3. multiply result no Expected")
+	}
+}
+
+func TestDivided(t *testing.T) {
+	if divided(30, 6) != 5 {
+		t.Error("1. divided result no Expected")
+	}
+	if divided(7, 4) != 1.75 {
+		t.Error("2. divided result no Expected")
+	}
+	if divided(-10, 2) != -5 {
+		t.Error("3. divided result no Expected")
+	}
+}
